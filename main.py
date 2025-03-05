6600070ce5a5d2c6a0b108a3fbf43cfa1b1f1c06
Julsvor <70030528+catgaming49@users.noreply.github.com>
2025-03-05 13:55:43 +0100

    Added initial files

---
+++ main.py
@@ -0,0 +1,38 @@
+from gpiozero import InputDevice, OutputDevice
+
+
+Pulse = InputDevice(2) # Pin 2, Active when dialing starts and inactiv when it isnt
+Dialing = InputDevice(3) # Pin 3, Pulses everytime it passes a number
+
+
+
+# When dialing starts Pulse can be accepted
+# Pulse is changed when dialing reached a new
+#
+#
+#
+#
+#
+#
+
+
+
+
+# Prototyping
+
+### Gets
+def get_ip_from_db():
+    pass
+
+def CallIP():
+    pass
+
+
+
+
+
+
+
+
+
+
