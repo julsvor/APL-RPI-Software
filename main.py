62f614d055b8dab37a1f94866232451c0d5be6bc
Julsvor <70030528+catgaming49@users.noreply.github.com>
2025-03-07 14:12:30 +0100

    Added analog to digital conversion, Added logging, Finished previous code

--- main.py
+++ main.py
@@ -1,48 +1,88 @@
 from gpiozero import InputDevice, OutputDevice
 import sqlite3
+import time
+import logging
+import ipaddress
 
-# Pulse = InputDevice(2) # Pin 2, Active when dialing starts and inactiv when it isnt
-# Dialing = InputDevice(3) # Pin 3, Pulses everytime it passes a number
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
+# logger.disabled = True
+
+Dial = InputDevice(pin=5, pull_up=False)
+Pulse = InputDevice(pin=6, pull_up=True)
+
+Dialing = 0
+
+timeout_len = 30 # Timeout in seconds between digits in a number, only activates if dialing is off so states arent mismatched
+
+number_length = 4 # Times out if not reached withing timeout_len, Stops dialing when reached
+
+number_arr = []
+
+timeout: int | None = None
+
+def resolve_number_to_ip(number:str) -> str | None:
+	logger.info("Resolving number: %s" % number)
+	con = sqlite3.connect("database.db")
+	cursor = con.cursor()
+
+	query = """
+	SELECT ip FROM phone_to_ip WHERE phone_number = ?;
+	"""
 
 
+	try:
+		result = cursor.execute(query, (number,))
+		result = result.fetchone()
 
-# When dialing starts Pulse can be accepted
-# Pulse is changed when dialing reached a new
-#
-#
-#
-#
-#
-#
+		if type(result) == tuple:
+			ip = str(ipaddress.ip_address(result[0]))
+			logger.info("Resolved number '%s' to IP adress '%s'" % (number, ip))
+			return ip
+		return result
+	except Exception as e:
+		logger.error("Failed to resolve: %s", e, exc_info=True)
 
-
-
-
-# Prototyping
-
-### Gets
-def resolve_number_to_ip(number):
-    con = sqlite3.connect("database.db")
-    cursor = con.cursor()
-
-    query = """
-    SELECT phone_number FROM phone_to_ip WHERE phone_number = ?;
-    """
-
-    result = cursor.execute(query, (number,))
-    result = result.fetchone()
-
-    if type(result) == tuple:
-        return result[0]
-
-    return result
-
-def CallIP():
+def CallIP(ip:str) -> None:
     pass
 
+while True:
+	if Dialing != Dial.value:
+		Dialing = Dial.value
+		if Dialing == 1:
+			logger.debug("Dialing started")
+			number = 0
+			number -= 1 # Remove first pulse
+			last_pulse = Pulse.value
+			while Dial.value:
+				if last_pulse != Pulse.value:
+					if last_pulse == 1:
+						logger.debug("Recieved Pulse")
+						number += 1
+				last_pulse = Pulse.value
+				time.sleep(0.001)
+			if number >= 0 and number <= 9:
+				number_arr.append(number)
+				if len(number_arr) >= number_length:
+					number_str = "".join([str(digit) for digit in number_arr])
+					logger.debug("Whole number recieved: %s" % number_str)
+					ip = resolve_number_to_ip(number_str)
+					number_arr.clear()
+					timeout = None
+			else:
+				logger.error("Invalid digit recieved: %s" % number)
 
-x = resolve_number_to_ip("4330")
-print(x)
+			logger.debug("Finished processing digit")
+		elif Dialing == 0:
+			timeout = time.time()
+			logger.debug("Dialing stopped")
+
+	if timeout != None and time.time() >= timeout + timeout_len:
+		logger.info("Dialing has timed out after '%s' seconds" % timeout_len)
+		timeout = None
+		number_arr.clear()
+
+	time.sleep(0.001)
 
 
 
